f38b09da4e9f116cf143751c841f31f1f09b7938
SimoneAcquadro <siyhfe@icloud.com>
2024-02-23 15:58:05 +0100

    metà mobile

--- m2/s1/g5/U2-D5/base_di_partenza/assets/js/script.js
+++ m2/s1/g5/U2-D5/base_di_partenza/assets/js/script.js
@@ -19,3 +19,15 @@ window.addEventListener("scroll", function () {
         }
     });
 });
+
+window.addEventListener('scroll', function() {
+    let fixedDiv = document.querySelector('.fixed');
+    let scrollPos = window.scrollY;
+
+    // Verifica se la posizione dello scroll è oltre una certa soglia
+    if (scrollPos > 800) { // Esempio: fissa il div dopo che lo scroll ha superato i 300 pixel
+        fixedDiv.classList.add('fixed_active'); // Aggiunge la classe per fissare il div
+    } else {
+        fixedDiv.classList.remove('fixed_active'); // Rimuove la classe quando si ritorna sotto la soglia
+    }
+});
